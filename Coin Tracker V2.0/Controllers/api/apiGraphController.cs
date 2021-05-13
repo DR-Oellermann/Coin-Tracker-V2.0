c7d91c7755009566df1f4c052d9abf8211f945d0
DrOellermann <u19057416@tuks.co.za>
2021-05-13 10:00:33 +0200

    Made new api controller that counts coins by type -- suprisingly works

---
+++ Coin Tracker V2.0/Controllers/api/apiGraphController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Coin_Tracker_V2._0.Models;
+
+namespace Coin_Tracker_V2._0.Controllers.api
+{
+    public class apiGraphController : ApiController
+    {
+        private SilverTrackerEntities db = new SilverTrackerEntities();
+        public IHttpActionResult GetcoinType()
+        {
+            var coinType = db.tblCoins.GroupBy(x => x.tblCoinType)
+                .Select(x => new {CoinType = x.Key.Type_Description, count = x.Count()}).ToList();
+
+            return Ok(coinType);
+        }
+    }
+}
